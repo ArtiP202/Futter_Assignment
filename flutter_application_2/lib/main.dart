cc93e33e6afbbe20cea61962c0b5f2e04bc542ce
ArtiP202 <artipawar2125@gmail.com>
2025-05-18 11:35:54 +0530

    Assignmet

---
+++ flutter_application_2/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner:false ,
+      home: Scaffold(
+        appBar: AppBar(
+        title: const Text(" Assignment 2"),
+      ),
+        body: Align(
+          alignment:Alignment.topLeft,
+          child: Container(
+           height:200,
+           width:200,
+           color:Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
+}
